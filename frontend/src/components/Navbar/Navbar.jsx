2c0491e196b80731e036f468b7539a998cfc2b32
hamzaejaz787 <hejaz389@gmail.com>
2022-12-01 10:28:13 +0500

    Navbar updated

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { HiMenu } from "react-icons/hi";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+
 import "./navbar.css";
 
+const Menu = () => (
+  <>
+    <li className="nav-links line">
+      <Link to="/about">Learn More</Link>
+    </li>
+    <li className="nav-links line">
+      <Link to="/support">Support</Link>
+    </li>
+    <li className="nav-links line">
+      <Link to="/login">Log In</Link>
+    </li>
+    <li className="nav-links">
+      <Link to="/signup">
+        <button className="sign-btn btn">Sign Up</button>
+      </Link>
+    </li>
+  </>
+);
+
 const Navbar = () => {
+  const [toggleNav, setToggleNav] = useState();
+
   return (
     <>
       <nav className="navbar">
@@ -11,27 +34,31 @@ const Navbar = () => {
         </Link>
 
         <ul className="navbar-lists">
-          <li className="nav-links line">
-            <Link to="/about">Learn More</Link>
-          </li>
-          <li className="nav-links line">
-            <Link to="/support">Support</Link>
-          </li>
-          <li className="nav-links line">
-            <Link to="/login">Log In</Link>
-          </li>
-          <li className="nav-links">
-            <Link to="/signup">
-              <button className="sign-btn btn">Sign Up</button>
-            </Link>
-          </li>
+          <Menu />
         </ul>
 
-        <ul className="navbar-lists-mobile">
+        <div className="navbar-lists-mobile">
           <li className="nav-links">
-            <HiMenu className="nav-toggle" />
+            {toggleNav ? (
+              <RiCloseLine
+                className="nav-toggle nav-toggle-close"
+                onClick={() => setToggleNav(false)}
+              />
+            ) : (
+              <RiMenu3Line
+                className="nav-toggle "
+                onClick={() => setToggleNav(true)}
+              />
+            )}
           </li>
-        </ul>
+          {toggleNav && (
+            <div className="navbar__lists_container">
+              <div className="navbar__lists_container-menu">
+                <Menu />
+              </div>
+            </div>
+          )}
+        </div>
       </nav>
     </>
   );
